e24d2b8e5c958496ae842780ff719144c46afca3
artsidorenko <artsidorenko@yandex-team.ru>
2024-02-27 22:23:09 +0300

    add abstraction over cli command

---
+++ src/main/java/hse/cli/commands/AbstractCommand.java
@@ -0,0 +1,17 @@
+package hse.cli.commands;
+
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
+public abstract class AbstractCommand implements Runnable {
+    protected final String[] arguments;
+    protected final PipedInputStream input;
+    protected final PipedOutputStream output;
+
+    public AbstractCommand(String[] args, PipedInputStream input, PipedOutputStream output) {
+        this.arguments = args;
+        this.input  = input;
+        this.output = output;
+    }
+
+}
