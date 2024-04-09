099864e7845856076f745743cb351b53cef50041
rpuwa <lilpushok@gmail.com>
2024-04-09 21:46:29 +0300

    noantlr

--- src/main/java/hse/cli/CLI.java
+++ src/main/java/hse/cli/CLI.java
@@ -1,45 +1,60 @@
 package hse.cli;
 
-import hse.cli.commands.AbstractCommand;
+import hse.cli.commands.*;
 import hse.manager.Manager;
-import hse.cli.antlr.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import java.util.Scanner;
 
 public class CLI {
     private static final InputStream input = System.in;
     private static final OutputStream output = System.out;
 
-    public static void main(String[] args) throws Exception {
-        CLILexer lexer = new CLILexer(CharStreams.fromStream(input));
-        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
-        CLIParser parser = new CLIParser(tokenStream);
-        parser.setBuildParseTree(true);
+    private static final String commandRegex = "[a-zA-Z][a-zA-Z0-9_-]*";
+    private static final String argRegex = "[a-zA-Z0-9_-]+";
 
-        CLIParser.InputContext inputContext = parser.input();
-        CLIMainListener listener = new CLIMainListener();
-        ParseTreeWalker walker = new ParseTreeWalker();
-        walker.walk(listener, inputContext);
+    private static AbstractCommand buildCommand(String name, List<String> args) {
+        return switch (name) {
+            case "cat" -> new CatCommand(args, null, null);
+            case "echo" -> new EchoCommand(args, null, null);
+            case "exit" -> new ExitCommand(args, null, null);
+            case "pwd" -> new PwdCommand(args, null, null);
+            case "wc" -> new WcCommand(args, null, null);
+            default -> new ExternalCommand(args, null, null);
+        };
+    }
 
-        List<AbstractCommand> commands = listener.getCommands();
-
-        if (commands == null) {
-            Manager.shutDown();
-            return;
+    private static List<AbstractCommand> getCommands(String line) {
+        List<AbstractCommand> result = new ArrayList<>();
+        for (String _token : line.split("\\|")) {
+            String token = _token.trim();
+            if (token.matches(String.format("(%s)(\\s+%s)", commandRegex, argRegex))) {
+                String[] cmd = token.split("\\s+");
+                String[] args = Arrays.copyOfRange(cmd, 1, cmd.length);
+                result.add(buildCommand(cmd[0], Arrays.asList(args)));
+            }
         }
+        return result;
+    }
 
-        Manager.startThreadPool();
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                List<AbstractCommand> commands = getCommands(scanner.nextLine());
+                if (!commands.isEmpty()) {
+                    Manager.startThreadPool();
 
-        InputStream finalInputStream = Manager.startPipeline(commands);
-        finalInputStream.transferTo(System.out);
-        Manager.shutDown();
+                    InputStream finalInputStream = Manager.startPipeline(commands);
+                    finalInputStream.transferTo(System.out);
+                    Manager.shutDown();
+                }
+            } catch (Exception e) {
+                return;
+            }
+        }
     }
 }
